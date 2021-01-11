76ebeef42189f0673d1fe047d331f58888fc6276
bt <biantaobrad@gmail.com>
2021-01-11 22:39:57 +0800

    css-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import addContent from './add-content.js'
+import './xuhuanyilai/index.js'
+
+import './style/index.css'
+
 document.write('My first Webpack app.<br />')
 addContent()
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
+    // filename: '[name]@[chunkhash].js',
+    filename: '[name].js',
+    // path: path.join(__dirname, 'dist') // webpack4 默认输出目录为dist
   },
   mode: 'development',
   devServer: {
     publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
+      }
+    ]
   }
 }
